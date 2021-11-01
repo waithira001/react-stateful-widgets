b41f4a7c7f5bb079744407903f3ed61de717e3cb
Ruth Muiruri <rmuiruri911@gmail.com>
2021-11-01 18:28:03 -0500

    Update Moods.js

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,22 +28,26 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
+  const [mood, setMood] = useState (initialMood);
   /* STEP 1 */
 
   const makeHappy = () => {
+    setMood(happyMood);
     /* STEP 4 */
   };
   const makeSad = () => {
+    setMood( sadMood);
     /* STEP 5 */
   };
   const reset = () => {
+    setMood (initialMood);
     /* STEP 6 */
   };
 
